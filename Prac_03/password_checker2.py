73c3f5f3aefcaa54bc0ab2b065e819a67dfb1448
jc497038 <curtis.lynch@my.jcu.edu.au>
2019-03-26 23:18:12 +1000

    Added main() function

--- Prac_03/password_checker2.py
+++ Prac_03/password_checker2.py
@@ -1,11 +1,13 @@
-password = input("What is password?")
+def program_1():
+    password = input("What is password?")
 
-min_length = 3
-password_length = len(password)
+    min_length = 3
+    password_length = len(password)
 
-while password_length < min_length:
-    print("Invalid")
+    while password_length < min_length:
+        print("Invalid")
     password = input(">")
     password_length = len(password)
 else:
-    print("*", password_length)
+        print("*", password_length)
+program_1()
