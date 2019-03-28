790d00abf5f1e5f89db6b282f2735ef1673ec530
Curtislynch <curtis.lynch@my.jcu.edu.au>
2019-03-28 13:06:29 +1000

    Added main() function

--- Prac_03/password_checker2.py
+++ Prac_03/password_checker2.py
@@ -1,13 +1,35 @@
+MIN_LENGTH = 3
+
+
+# password_length = len(password)
+
+# First version of password checker
 def program_1():
-    password = input("What is password?")
-
-    min_length = 3
-    password_length = len(password)
-
-    while password_length < min_length:
+    password = input("Enter a password of at least 3 characters: ".format(MIN_LENGTH))
+    while len(password) < MIN_LENGTH:
         print("Invalid")
-    password = input(">")
-    password_length = len(password)
-else:
-        print("*", password_length)
+        password = input("Enter a password of at least 3 characters: ".format(MIN_LENGTH))
+    else:
+        print('*' * len(password))
+
+
 program_1()
+
+
+def get_password(min_length):
+    password = input("Enter a password of at least 3 characters: ".format(min_length))
+    while len(password) < min_length:
+        print("Invalid")
+        password = input("Enter a password of at least 3 characters: ".format(min_length))
+    return password
+
+
+def print_asterisks(sequence):
+    print('*' * len(sequence))
+
+
+def main():
+    password = get_password(MIN_LENGTH)
+    print_asterisks(password)
+
+main()
